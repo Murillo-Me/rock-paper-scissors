a53d3d1aa3646bafe873ebf5778811dcdcfcda0b
Murillo Mendes <murillomendes95@hotmail.com>
2021-09-14 00:36:15 -0300

    Core functionalities

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # rock-paper-scissors
 
-This is the fist javascript project on The Odin Project's Foundations curriculum.
+This is the fist javascript project on The Odin Project's Foundations curriculum, now with a UI.
 A simple rock, paper, scissor game.
--- scripts/app.js
+++ scripts/app.js
@@ -4,14 +4,6 @@ function computerPlay() {
     return possibleSelections[Math.floor(Math.random() * possibleSelections.length)]
 }
 
-function playerPlay() {
-    let playerPick = prompt('What is your play? Rock, paper or scissors?').toLowerCase()
-    while (playerPick !== 'rock' && playerPick !== 'paper' && playerPick !== 'scissors') {
-        playerPick = prompt(`${playerPick} is not a valid option. Choose again.`).toLowerCase()
-    }
-    return playerPick
-}
-
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return {result: `It's a draw! You have both picked ${playerSelection}`, winner: 'draw'}
@@ -54,5 +46,70 @@ function game() {
     }
 }
 
-alert("You are going to play 5 rounds of rock, paper, scissors agaisn't the computer. Are you ready?");
-game();
+function countScore(winner, playerScore, computerScore) {
+
+        if (winner !== 'draw') {
+            winner === 'player' ? playerScore++ : computerScore++
+        }
+
+        // if (playerScore >= 3) {
+        //     alert(`You win! You have won ${playerScore} rounds and lost ${computerScore}.`)
+        //     return
+        // }
+        // else if (computerScore >= 3) {
+        //     alert(`You lose! You have lost ${computerScore} rounds and won ${playerScore}.`)
+        //     return
+        // }
+}
+
+const announcements = document.querySelector('.instructions:first-of-type')
+const result = document.querySelector('.result')
+console.log(announcements)
+
+announcements.textContent = "You are going to play 5 rounds of rock, paper, scissors agaisn't the computer."
+announcements.innerHTML += '<br>What is your play? Rock, paper or scissors?<br><br>'
+
+const buttons = document.querySelectorAll('.user-input')
+
+const computerScoreElement = document.querySelector('#computer-score')
+let computerScore = 0
+
+const playerScoreElement = document.querySelector('#player-score')
+let playerScore = 0
+
+buttons.forEach(btn => {btn.addEventListener('click', (e) => {
+    let roundResults = playRound(e.currentTarget.getAttribute('id'),computerPlay())
+    result.textContent = roundResults.result
+    console.log(roundResults)
+
+    if (roundResults.winner==='draw') return;
+    (roundResults.winner==='player') ? playerScore++ : computerScore++
+    
+    playerScoreElement.textContent = playerScore
+    computerScoreElement.textContent = computerScore
+
+    if (playerScore === 5) {
+        alert("YOU HAVE WON! CONGRATULATIONS!")
+        window.location.reload()
+    }
+    
+    if (computerScore === 5) {
+        alert("YOU HAVE LOST! TRY AGAIN NEXT TIME!")
+        window.location.reload()
+    }
+})})
+
+
+
+// buttons.forEach(btn => {btn.addEventListener('click', (e) => {
+//     playerPick = e.currentTarget
+//     console.log(playerPick)
+// })})
+
+// setTimeout(() => {
+//     announcements.textContent = "You are going to play 5 rounds of rock, paper, scissors agaisn't the computer."
+//     console.log(playerPlay(announcements))
+// }, 2000);
+
+// alert("You are going to play 5 rounds of rock, paper, scissors agaisn't the computer.");
+// game();
